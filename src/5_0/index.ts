280555b75a22d2180ff43e403ca6de9c89767b5d
Blake Newman <code@blakenewman.dev>
2023-04-15 16:09:07 +0100

    feat: improve auto import provider (#2)
    * feat: improve auto import provider
    
    Ensure that auto import provider only clears cache when referencing symlink has
    changed, previously it would reset the cache when any changes to projects where
    made. This could be slow to pick up auto imports as it would trigger auto import
    to resolve every time change was made.
    
    Copy more source types over from source to make the code more typesafe and easier
    to reason with.
    
    Decouple auto import provider project to seperate file to make easier to work with.
    
    * fix: `key in` always false in some properties for proxy host
    
    * fix: explicitly proxy methods to host
    
    Explicitly proxy project methods to host, init project (constructor) seperately as this would proxy
    twice for auto import provider and allows seperate logic for creating language service.
    
    * refactor: remove uneeded methods from project
    
    ---------
    
    Co-authored-by: johnsoncodehk <johnsoncodehk@gmail.com>

--- src/5_0/index.ts
+++ src/5_0/index.ts
@@ -1,9 +1,10 @@
 import { createProjectService, ProjectService } from './projectService';
-import { createProject } from './project';
+import { createProject, initProject, Project } from './project';
 import type { LanguageService, LanguageServiceHost, UserPreferences } from 'typescript/lib/tsserverlibrary';
 
 // only create the once for all hosts, as this will improve performance as the internal cache can be reused
 let projectService: ProjectService;
+const projects = new Set<Project>()
 
 export default function (
 	ts: typeof import('typescript/lib/tsserverlibrary'),
@@ -13,7 +14,6 @@ export default function (
 ) {
 	const hostConfiguration = { preferences: { includePackageJsonAutoImports: 'auto' } as UserPreferences };
 
-	// will need to make this the workspace directory
 	if (!projectService) {
 		projectService = createProjectService(
 			ts,
@@ -30,15 +30,24 @@ export default function (
 		createLanguageService,
 		{
 			projectService,
-			rootNames: host.getScriptFileNames(),
 			currentDirectory: host.getCurrentDirectory(),
 			compilerOptions: host.getCompilationSettings(),
 		}
 	);
 
-	// Immediatly invoke so the language service provider is setup
-	// this preinitialises getting auto imports in IDE
-	project.getPackageJsonAutoImportProvider();
+	const proxyMethods: (keyof Project)[] = [
+		'getCachedExportInfoMap',
+		'getModuleSpecifierCache',
+		'getGlobalTypingsCacheLocation',
+		'getSymlinkCache',
+		'getPackageJsonsVisibleToFile',
+		'getPackageJsonAutoImportProvider',
+		'includePackageJsonAutoImports',
+		'useSourceOfProjectReferenceRedirect'
+	]
+	proxyMethods.forEach(key => (host as any)[key] = project[key].bind(project))
+	initProject(project, host, createLanguageService)
+	projects.add(project)
 
 	return {
 		languageService: project.languageService!,
@@ -48,6 +57,18 @@ export default function (
 			if (onAutoImportProviderSettingsChanged) {
 				project.onAutoImportProviderSettingsChanged();
 			}
+		},
+		projectUpdated(path: string) {
+			projects.forEach(projectToUpdate => {
+				if (project === projectToUpdate || !projectToUpdate.autoImportProviderHost) return
+
+				const realPaths = [...projectToUpdate.symlinks?.getSymlinkedDirectoriesByRealpath()?.keys() ?? []]
+					.map(name => projectToUpdate.projectService.getNormalizedAbsolutePath(name));
+				
+				if (realPaths.includes(projectToUpdate.projectService.toCanonicalFileName(path))) {
+					projectToUpdate.autoImportProviderHost.markAsDirty();
+				}
+			})
 		},
 	};
 }
